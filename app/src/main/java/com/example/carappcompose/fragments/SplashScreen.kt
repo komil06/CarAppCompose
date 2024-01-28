1c9f68a11065ab73efc66712a96b6cd374c4a52a
SN-20061230 <sardorbek2006.normurodov@gmail.com>
2024-01-28 09:05:58 +0500

    KomilChik

--- app/src/main/java/com/example/carappcompose/fragments/SplashScreen.kt
+++ app/src/main/java/com/example/carappcompose/fragments/SplashScreen.kt
@@ -1,12 +1,15 @@
 package com.example.carappcompose.fragments
 
+import android.view.animation.OvershootInterpolator
 import android.window.SplashScreen
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,16 +22,35 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(navController:NavController){
+fun SplashScreen(navController: NavController) {
     val context = LocalContext.current
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
     LaunchedEffect(key1 = true) {
-        delay(6000)
+        scale.animateTo(
+            targetValue =1.5f,
+            animationSpec = tween(
+                durationMillis = 4000,
+                easing = {
+
+                    OvershootInterpolator(1f).getInterpolation(it)
+                })
+        )
+        delay(3000)
+
         navController.navigate("SignIn")
 
     }
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color(255, 92, 0)),contentAlignment = Alignment.Center,){
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+        ,
+        contentAlignment = Alignment.Center,
+    ) {
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.Url("https://lottie.host/2efb0d40-0e69-4566-b718-400d5a88fcbb/IyXFFjxjPD.lottie"))
+        LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever)
     }
 }
-
