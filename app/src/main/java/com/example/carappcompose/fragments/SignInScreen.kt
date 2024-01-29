bc7418f0e681aefefc5471ec8f3a497f432089bd
komil <komashuxratov@gmail.com>
2024-01-29 09:59:04 +0500

    server

--- app/src/main/java/com/example/carappcompose/fragments/SignInScreen.kt
+++ app/src/main/java/com/example/carappcompose/fragments/SignInScreen.kt
@@ -2,6 +2,7 @@ package com.example.carappcompose.fragments
 
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -28,9 +30,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -39,7 +43,7 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SignInScreen(){
+fun SignInScreen(navController: NavController){
     Surface(modifier = Modifier
         .fillMaxSize()
         .padding(10.dp)
@@ -47,30 +51,40 @@ fun SignInScreen(){
         Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             val composition by rememberLottieComposition(spec = LottieCompositionSpec.Url("https://lottie.host/3ae5a037-9bbf-4c01-88f5-5b0800a40d67/h2uAKVCBce.lottie"))
             LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever) }
-        Column(modifier = Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally){
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
             Spacer(modifier = Modifier.height(10.dp))
             Text(text = "Login")
             Spacer(modifier = Modifier.height(10.dp))
             Text(text = "Welcome to CarStore")
-            var text by remember{ mutableStateOf("") }
+            var text by remember { mutableStateOf(TextFieldValue("")) }
+            var password by remember { mutableStateOf(TextFieldValue("")) }
             OutlinedTextField(
                 value = text,
                 leadingIcon = {Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null, modifier = Modifier.padding(8.dp)) },
                 onValueChange = { text = it },
                 label = { Text("Username") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+
+                        shape = RoundedCornerShape(12.dp)
             )
             Spacer(modifier = Modifier.height(10.dp))
             OutlinedTextField(
-                value = text,
+                value = password,
                 leadingIcon = {Icon(imageVector = Icons.Default.Lock, contentDescription = null, modifier = Modifier.padding(8.dp)) },
-                onValueChange = { text = it },
+                onValueChange = { password = it },
                 label = { Text("Password") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                shape = RoundedCornerShape(12.dp)
+
+
+
+
             )
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             Text(text="Forgot Password?")
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             Button(modifier = Modifier.padding(top = 27.dp),onClick = {}){
                 Text(
                     modifier = Modifier.padding(horizontal = 15.dp, vertical = 5.dp),
@@ -78,7 +92,7 @@ fun SignInScreen(){
                     fontSize = 19.sp
                 )
             }
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(30.dp))
             Row{
                 Text(text="Don't have an acoount?")
 
@@ -93,8 +107,8 @@ fun SignInScreen(){
 
     }
 }
-@Preview
-@Composable
-fun SingInPreview(){
-    SignInScreen()
-}
+//@Preview
+//@Composable
+//fun SingInPreview(){
+//    SignInScreen()
+//}
