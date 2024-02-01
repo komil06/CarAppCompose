17c4bee0b4d91f8811e6de02c160ef4666a0a4d7
komil <komashuxratov@gmail.com>
2024-02-01 09:59:51 +0500

    Komilchik

--- app/src/main/java/com/example/carappcompose/fragments/MainScreen.kt
+++ app/src/main/java/com/example/carappcompose/fragments/MainScreen.kt
@@ -1,121 +1,160 @@
 package com.example.carappcompose.fragments
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-
+import com.example.carappcompose.ui.theme.poppinsFamily
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavController){
-//   Scaffold (
-//       topBar = {
-//           TopAppBar(navigationIcon = {
-//               IconButton(onClick = {}, modifier = Modifier.width(30.dp).height(30.dp)) {
-//                   Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu Icon")
-//               }
-//               Text(text = "CarStore", modifier = Modifier.padding(30.dp))
-//               IconButton(onClick = {}) {
-//                   Icon(imageVector = Icons.Default.Notifications, contentDescription = "Notifications")
-//               }
-//           },
-//               title = {
-//                   Text(text = "")
-//               })
-//       }
-//   ){
-//
-//   }
-
+    var presses by remember { mutableIntStateOf(0) }
 
     Scaffold(
-
         topBar = {
-           TopAppBar( modifier = Modifier.fillMaxWidth(), navigationIcon = {
-               IconButton(onClick = {}, modifier = Modifier.width(30.dp).height(30.dp)) {
-                   Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu Icon")
-               }
-               Text(text = "CarStore", modifier = Modifier.padding(30.dp))
-               IconButton(onClick = {}) {
-                   Icon(imageVector = Icons.Default.Notifications, contentDescription = "Notifications")
-               }
-           },
-               title = {
-                   Text(text = "")
-               })
-       }, bottomBar = {
-            BottomAppBar(modifier = Modifier.height(70.dp).padding(start = 50.dp)) {
+            CenterAlignedTopAppBar(
+//                colors = TopAppBarDefaults.topAppBarColors(
+//                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                    titleContentColor = MaterialTheme.colorScheme.primary,
+//                ),
+                title = {
+                    Text(
+                        "CarStore",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        fontFamily = poppinsFamily,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+//                scrollBehavior = scrollBehavior,
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+//                containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                contentColor = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .padding(bottom = 40.dp, start = 20.dp, end = 20.dp)
+                    .clip(
+                        RoundedCornerShape(30.dp)
+                    ).fillMaxWidth().height(65.dp),
 
 
-//                Row(modifier = Modifier.fillMaxWidth().height(80.dp)){
+                    containerColor = Color.White
 
 
 
 
 
-//                IconButton(modifier = Modifier.fillMaxSize(), onClick = { navController.navigate("Settings") }) {
-//                    Icon(
-//                        imageVector = Icons.Default.Settings,
-//                        contentDescription = "Details Icon",
-//                        modifier = Modifier.width(50.dp).height(50.dp)
-//                    )
-//                    Icon(
-//                        imageVector = Icons.Default.Settings,
-//                        contentDescription = "Details Icon",
-//                        modifier = Modifier.width(50.dp).height(50.dp)
-//                    )
-//                    Icon(
-//                        imageVector = Icons.Default.Settings,
-//                        contentDescription = "Details Icon",
-//                        modifier = Modifier.width(50.dp).height(50.dp)
-//                    )
-//
-//                }
 
+            ) {
+
+
+
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround){
+                    Icon(imageVector = Icons.Default.Home, contentDescription = null, modifier = Modifier.width(40.dp).height(40.dp),
+
+
+                        )
+                    Icon(imageVector = Icons.Default.Favorite, contentDescription = null, modifier = Modifier.width(40.dp).height(40.dp),
+
+
+                        )
+                    Icon(imageVector = Icons.Default.Person, contentDescription = null, modifier = Modifier.width(40.dp).height(40.dp),
+
+
+                        )
+
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { presses++ }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
             }
         }
-
-
-
-
-
     ) { innerPadding ->
-        LazyColumn(
+        Column(
             modifier = Modifier
-                .fillMaxSize()
                 .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-//            items() { item ->
-//            }
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+
+                    It also contains some basic inner content, such as this text.
+
+                    You have pressed the floating action button $presses times.
+                """.trimIndent(),
+            )
         }
-
-
-
     }
 }
